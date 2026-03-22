e9d7010e6cabac2012dc39b460a73f8cf9c6bba1
Atul Kamble <atul_kamble@hotmail.com>
2026-03-22 15:23:57 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.65.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "08b7b8d4-af42-4972-9517-11ea256ea068"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-sql"
+  location = "australiacentral"
+}
+
+
+resource "azurerm_mssql_server" "sql_server" {
+  name                         = "sqlserver2024"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = "P@ssw0rd1234"
+}
+
+resource "azurerm_mssql_database" "sql_database" {
+  name      = "sqldatabase2024"
+  server_id = azurerm_mssql_server.sql_server.id
+  sku_name  = "Basic"
+}
