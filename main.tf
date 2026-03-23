a3415bb27bed5323691b82177f312dafda2f2a90
Atul Kamble <atul_kamble@hotmail.com>
2026-03-23 06:44:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,23 +12,27 @@ provider "azurerm" {
   subscription_id = "08b7b8d4-af42-4972-9517-11ea256ea068"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-sql"
-  location = "australiacentral"
-}
-
-
-resource "azurerm_mssql_server" "sql_server" {
-  name                         = "sqlserver2024"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  version                      = "12.0"
-  administrator_login          = "sqladmin"
-  administrator_login_password = "P@ssw0rd1234"
-}
-
-resource "azurerm_mssql_database" "sql_database" {
-  name      = "sqldatabase2024"
-  server_id = azurerm_mssql_server.sql_server.id
-  sku_name  = "Basic"
-}
+resource "azurerm_resource_group" "rg" { 
+   name     = "rg-sql" 
+   location = "australiacentral" 
+ } 
+  
+ resource "azurerm_mssql_server" "sql_server" { 
+   name                         = "sqlserver2024-${random_id.server_suffix.hex}" 
+    resource_group_name          = azurerm_resource_group.rg.name 
+   location                     = azurerm_resource_group.rg.location 
+   version                      = "12.0" 
+   administrator_login          = "sqladmin" 
+   administrator_login_password = "P@ssw0rd1234" 
+ } 
+   
+ resource "random_id" "server_suffix" { 
+   byte_length = 2 
+ } 
+  
+  
+ resource "azurerm_mssql_database" "sql_database" { 
+   name      = "sqldatabase2024" 
+   server_id = azurerm_mssql_server.sql_server.id 
+   sku_name  = "Basic" 
+ }
